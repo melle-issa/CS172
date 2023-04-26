cf7e3667921e2691e32607f10af80dccddc4f6b3
Melissa Hidalgo <mhida010@ucr.edu>
2023-04-26 15:05:59 -0700

    Started main file for crawling

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#before you do pip3 install beautifulsoup4 and pip3 install lxml first
+
 from bs4 import BeautifulSoup
 
-with open('home.html', 'r')
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
