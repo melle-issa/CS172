5a0a5fe6ef6238bc3bb6d2205e5ef30ddf153178
Melissa Hidalgo <mhida010@ucr.edu>
2023-04-27 19:55:09 -0700

    need to handle duplicate pages

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-#before you do pip3 install beautifulsoup4 and pip3 install lxml first
+# before running, please open your terminal and type:
+# pip3 install beautifulsoup4
+# pip3 install lxml
+# pip3 install requests
 
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
+def crawl(seedFileName, levels):
+
+    with open(seedFileName, 'r') as seed:
+        urls = seed.readlines()
+        
+    clicks_away = 0
+    limit = levels
+    count = 0
+    frontier = []
+    visited = []
+        
+    for link in urls:
+        frontier.append(link)
+        visited.append(link)
+
+    while frontier != [] and clicks_away < limit:
+        for link in frontier:
+            html_frontier = requests.get(link).text
+            outputName = "crawled_pages/htmlFile" + str(count) + ".html"
+            output = open(outputName, "w")
+            output.write(html_frontier)
+            output.close()
+            count = count + 1
+            frontier.pop(0)
+            soup = BeautifulSoup(html_frontier, 'lxml')
+            links = soup.find_all('a')
+            for website in links:
+                url = website.get('href')
+                if url and url.startswith('http'):
+                    if url not in visited:
+                        frontier.append(url)
+                        visited.append(url)
+            if len(frontier) > 500 : break
+        clicks_away = clicks_away + 1
+
+crawl("seed.txt", 5)
